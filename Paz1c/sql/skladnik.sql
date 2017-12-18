35842625ce282cd7cccea3633185298807a022f6
Matus <Marcel@MarcelPC>
2017-12-18 23:30:24 +0100

    zaukruhlovanie, pozri potom ten DecimalFormat

--- Paz1c/sql/skladnik.sql
+++ Paz1c/sql/skladnik.sql
@@ -5,19 +5,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema skladnik
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema skladnik
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `skladnik` DEFAULT CHARACTER SET utf8 ;
+USE `skladnik` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`Sklad`
+-- Table `skladnik`.`Sklad`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Sklad` (
+CREATE TABLE IF NOT EXISTS `skladnik`.`Sklad` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `Nazov` VARCHAR(45) NULL,
   `Adresa` VARCHAR(45) NULL,
@@ -26,9 +26,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Pouzivatel`
+-- Table `skladnik`.`Pouzivatel`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Pouzivatel` (
+CREATE TABLE IF NOT EXISTS `skladnik`.`Pouzivatel` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `Meno` VARCHAR(45) NULL,
   `heslo` VARCHAR(45) NULL,
@@ -38,16 +38,54 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Pouzivatel` (
   UNIQUE INDEX `Meno_UNIQUE` (`Meno` ASC),
   CONSTRAINT `fk_Pouzivatel_Sklad1`
     FOREIGN KEY (`Sklad_id`)
-    REFERENCES `mydb`.`Sklad` (`id`)
+    REFERENCES `skladnik`.`Sklad` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Material`
+-- Table `skladnik`.`Prijemka`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Material` (
+CREATE TABLE IF NOT EXISTS `skladnik`.`Prijemka` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `cena` DOUBLE NULL,
+  `Pouzivatel_id` INT NOT NULL,
+  `datum` DATETIME NULL,
+  `typ_pohybu` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_Prijemka_Pouzivatel_idx` (`Pouzivatel_id` ASC),
+  CONSTRAINT `fk_Prijemka_Pouzivatel`
+    FOREIGN KEY (`Pouzivatel_id`)
+    REFERENCES `skladnik`.`Pouzivatel` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `skladnik`.`Vydajka`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `skladnik`.`Vydajka` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `Pouzivatel_id` INT NOT NULL,
+  `cena` DOUBLE NULL,
+  `datum` DATETIME NULL,
+  `typ_pohybu` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_Prijemka_Pouzivatel_idx` (`Pouzivatel_id` ASC),
+  CONSTRAINT `fk_Prijemka_Pouzivatel0`
+    FOREIGN KEY (`Pouzivatel_id`)
+    REFERENCES `skladnik`.`Pouzivatel` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `skladnik`.`Material`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `skladnik`.`Material` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `Nazov` VARCHAR(45) NULL,
   `Stav` INT NULL,
@@ -57,57 +95,33 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Material` (
   INDEX `fk_Material_Sklad1_idx` (`Sklad_id` ASC),
   CONSTRAINT `fk_Material_Sklad1`
     FOREIGN KEY (`Sklad_id`)
-    REFERENCES `mydb`.`Sklad` (`id`)
+    REFERENCES `skladnik`.`Sklad` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Prijemka`
+-- Table `skladnik`.`pohybMaterialu`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Prijemka` (
+CREATE TABLE IF NOT EXISTS `skladnik`.`pohybMaterialu` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `Pocet` INT NULL,
+  `id_materialu` INT NULL,
+  `pocet` INT NULL,
   `cena` DOUBLE NULL,
-  `Pouzivatel_id` INT NOT NULL,
-  `Material_id` INT NOT NULL,
+  `Prijemka_id` INT NULL,
+  `Vydajka_id` INT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_Prijemka_Pouzivatel_idx` (`Pouzivatel_id` ASC),
-  INDEX `fk_Prijemka_Material1_idx` (`Material_id` ASC),
-  CONSTRAINT `fk_Prijemka_Pouzivatel`
-    FOREIGN KEY (`Pouzivatel_id`)
-    REFERENCES `mydb`.`Pouzivatel` (`id`)
+  INDEX `fk_pohybMaterialu_Prijemka1_idx` (`Prijemka_id` ASC),
+  INDEX `fk_pohybMaterialu_Vydajka1_idx` (`Vydajka_id` ASC),
+  CONSTRAINT `fk_pohybMaterialu_Prijemka1`
+    FOREIGN KEY (`Prijemka_id`)
+    REFERENCES `skladnik`.`Prijemka` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Prijemka_Material1`
-    FOREIGN KEY (`Material_id`)
-    REFERENCES `mydb`.`Material` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`Vydajka`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Vydajka` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `Pocet` INT NULL,
-  `Pouzivatel_id` INT NOT NULL,
-  `Material_id` INT NOT NULL,
-  `cena` DOUBLE NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Prijemka_Pouzivatel_idx` (`Pouzivatel_id` ASC),
-  INDEX `fk_Vydajka_Material1_idx` (`Material_id` ASC),
-  CONSTRAINT `fk_Prijemka_Pouzivatel0`
-    FOREIGN KEY (`Pouzivatel_id`)
-    REFERENCES `mydb`.`Pouzivatel` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Vydajka_Material1`
-    FOREIGN KEY (`Material_id`)
-    REFERENCES `mydb`.`Material` (`id`)
+  CONSTRAINT `fk_pohybMaterialu_Vydajka1`
+    FOREIGN KEY (`Vydajka_id`)
+    REFERENCES `skladnik`.`Vydajka` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
